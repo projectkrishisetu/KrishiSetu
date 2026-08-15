29cfbd9b907db0072da7f364a12f888f9b0b545e
projectkrishisetu <projectkrishisetu@gmail.com>
2026-08-15 20:51:48 +0530

    chore: migrate to stable tailwind v3 & fix postcss configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
